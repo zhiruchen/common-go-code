5839f35a2bfbcdf7c01a255be12cdb1c377f2950
zhiruchen <zhiruchen1992@foxmail.com>
2020-02-19 09:41:55 +0800

    base64 str to file

---
+++ based64/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func base64ToImgBytes(base64Str string) ([]byte, error) {
+	unbased, err := base64.StdEncoding.DecodeString(base64Str)
+	if err != nil {
+		return nil, err
+	}
+	return unbased, nil
+}
+
+func main() {
+	base64Str = ``
+	imgBytes, err := base64ToImgBytes(base64Str)
+	if err != nil {
+		fmt.Print("based64 decode error: ", err)
+		os.Exit(1)
+	}
+
+	err = ioutil.WriteFile("test_file", imgBytes, 0644)
+	fmt.Println("write file error: ", err)
+}
